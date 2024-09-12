6f3aaf216ec7755140fba0547257b77186526e14
endoxide <matefehervari24@gmail.com>
2024-09-13 00:58:55 +0100

    Make HOME environment variable croos-platform

--- lua/endoxide/options.lua
+++ lua/endoxide/options.lua
@@ -30,7 +30,7 @@ vim.opt.wrap = false
 
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.local/share/nvim/undodir"
+vim.opt.undodir = vim.env.HOME .. "/.local/share/nvim/undodir"
 vim.opt.undofile = true
 
 vim.opt.hlsearch = false
