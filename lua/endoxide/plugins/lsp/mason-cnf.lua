b908ad8fff3b6a913b41f25a6cc1f78cbe97f8a3
Máté Fehérvári <matefehervari24@gmail.com>
2025-05-21 10:05:54 +0100

    Add Mason open keybind and require installed

--- lua/endoxide/plugins/lsp/mason-cnf.lua
+++ lua/endoxide/plugins/lsp/mason-cnf.lua
@@ -2,11 +2,14 @@ return {
     "williamboman/mason.nvim",
     config = function ()
         local mason = require("mason")
+        local nnoremap = require("endoxide.keymap").nnoremap
         local config = {
             ui = {
                 border = "rounded"
             }
         }
         mason.setup(config)
+
+        nnoremap("<leader>fm", "<Cmd>Mason<CR>", {desc = "Open Mason"})
     end
 }
--- lua/endoxide/plugins/lsp/mason-lspconfig-conf.lua
+++ lua/endoxide/plugins/lsp/mason-lspconfig-conf.lua
@@ -4,9 +4,41 @@ return {
         "williamboman/mason.nvim",
         "neovim/nvim-lspconfig",
     },
-    config = function ()
+    config = function()
         local mason_lsp = require("mason-lspconfig")
-        mason_lsp.setup()
+
+        local config = {
+            ensure_installed = {
+                -- Scripting
+                "bashls",    -- Bash
+                -- "checkmake", -- Makefile
+
+                -- Webdev
+                "cssls",                 -- CSS
+                "cssmodules_ls",         -- CSS Modules
+                "emmet_language_server", -- Emmet
+                "ts_ls",                 -- Typescript
+
+                -- Langs
+                "clangd",        -- C, C++
+                "hls",           -- Haskell
+                "jdtls",         -- Java
+                "lua_ls",        -- Lua
+                "ocamllsp",      -- Ocaml
+                "omnisharp",     -- C#
+                "pyright",       -- Python
+                "rust_analyzer", -- Rust
+                "texlab",        -- Tex
+
+                -- Data
+                "jsonls",  -- Json
+
+                -- Format
+                "ruff",  -- Python
+            }
+        }
+
+        mason_lsp.setup(config)
         mason_lsp.setup_handlers({
             require("endoxide.lsp.handlers")
         })
