c874ea640942af0fa47512213fbd1e428d31846d
Máté Fehérvári <matefehervari24@gmail.com>
2025-08-20 23:55:20 +0100

    Formatting

--- lua/endoxide/lazy.lua
+++ lua/endoxide/lazy.lua
@@ -2,55 +2,51 @@ local fn = vim.fn
 
 local lazypath = fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.uv.fs_stat(lazypath) then
-  LAZY_BOOTSTRAP = fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    LAZY_BOOTSTRAP = fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 local plugins = {
-  "nvim-lua/popup.nvim", -- An implementation of the Popup API from vim in Neovim
-  {import = "endoxide.plugins"},
-  {import = "endoxide.plugins.lsp"},
+    "nvim-lua/popup.nvim", -- An implementation of the Popup API from vim in Neovim
+    { import = "endoxide.plugins" },
+    { import = "endoxide.plugins.lsp" },
 
-  -- snippets
-  "rafamadriz/friendly-snippets", -- a bunch of snippets to use
+    -- snippets
+    "rafamadriz/friendly-snippets", -- a bunch of snippets to use
 
-  -- lsp stuff
-  -- "neovim/nvim-lspconfig", -- enable LSP
-  "williamboman/mason.nvim", -- LSP installer
-  "williamboman/mason-lspconfig.nvim",
+    -- lsp stuff
+    -- "neovim/nvim-lspconfig", -- enable LSP
+    "williamboman/mason.nvim", -- LSP installer
+    "williamboman/mason-lspconfig.nvim",
 
-  "jose-elias-alvarez/null-ls.nvim",
+    "jose-elias-alvarez/null-ls.nvim",
 
-  -- Telescope
-  "nvim-telescope/telescope-media-files.nvim",
-
-
-  -- use "lewis6991/spellsitter.nvim"
-  "JoosepAlviste/nvim-ts-context-commentstring",
-
-  -- DAP
-  "mfussenegger/nvim-dap",
-  {
-    "rcarriga/nvim-dap-ui",
-     dependencies = {"mfussenegger/nvim-dap"}
-  },
-
--- Python
-  "mfussenegger/nvim-dap-python",
-
-  -- Java
-  "mfussenegger/nvim-jdtls",
+    -- Telescope
+    "nvim-telescope/telescope-media-files.nvim",
 
 
-  -- Rust
-  "simrat39/rust-tools.nvim",
+    -- use "lewis6991/spellsitter.nvim"
+    "JoosepAlviste/nvim-ts-context-commentstring",
+
+    -- DAP
+    "mfussenegger/nvim-dap",
+    {
+        "rcarriga/nvim-dap-ui",
+        dependencies = { "mfussenegger/nvim-dap" }
+    },
+
+    -- Python
+    "mfussenegger/nvim-dap-python",
+
+    -- Java
+    "mfussenegger/nvim-jdtls",
 }
 
 local opts = {
