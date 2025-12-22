3898b39635424c8adf84d59980385d1c4820a906
Máté Fehérvári <38787928+matefehervari@users.noreply.github.com>
2025-12-22 15:22:56 +0000

    Add ipkg to ignored treesitter parsers

--- lua/endoxide/plugins/treesitter-cnf.lua
+++ lua/endoxide/plugins/treesitter-cnf.lua
@@ -11,9 +11,9 @@ return {
         local configs = {
             ensure_installed = "all",
             sync_install = false,
-            ignore_install = { "" },  -- List of parsers to ignore installing
+            ignore_install = { "ipkg" },
             highlight = {
-                enable = true,        -- false will disable the whole extension
+                enable = true,                -- false will disable the whole extension
                 disable = { "latex", "lua" }, -- list of language that will be disabled
                 additional_vim_regex_highlighting = true,
             },
@@ -22,7 +22,7 @@ return {
                     enable = true,
                     clear_on_cursor_move = false,
                 },
-            }
+            },
         }
 
         ts.setup(configs)
